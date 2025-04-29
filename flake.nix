09e2563c0242dce8ba412066f4d056af26e44a8d
Genzo Namikawa <genzo.namikawa@outlook.com>
2025-04-30 02:18:47 +0900

    feat: enable openssh

--- flake.nix
+++ flake.nix
@@ -22,6 +22,17 @@
 
             imports = [ ./hardware-configuration.nix ]; # Keep hardware config separate
 
+            services.openssh = {
+              enable = true;
+              ports = [ 22 ];
+              settings = {
+                PasswordAuthentication = true;
+                UseDns = true;
+                X11Forwarding = false;
+                PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
+              };
+            };
+
             boot.loader.systemd-boot.enable = true;
             boot.loader.efi.canTouchEfiVariables = true;
 
