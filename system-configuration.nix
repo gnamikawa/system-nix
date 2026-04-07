3fb05c3d82c8c410636dcb10542cfda4ccabfbc2
Genzo Namikawa <genzo.namikawa@outlook.com>
2026-04-08 03:17:00 +0900

    fix: remove fcitx5 inputMethod
    Removing the system-level fcitx5 configuration hands sole ownership of
    input method setup to home-manager, preventing conflicts between the
    NixOS module and home-manager declarations for the same option.

--- system-configuration.nix
+++ system-configuration.nix
@@ -71,19 +71,6 @@
   i18n = {
     defaultLocale = "en_US.UTF-8";
     extraLocales = [ "ja_JP.UTF-8/UTF-8" ];
-    inputMethod = {
-      enable = true;
-      type = "fcitx5";
-      enableGtk2 = true;
-      enableGtk3 = true;
-      fcitx5 = {
-        addons = with pkgs; [
-          fcitx5-gtk
-          fcitx5-mozc-ut
-        ];
-        waylandFrontend = true;
-      };
-    };
   };
 
   security = {
